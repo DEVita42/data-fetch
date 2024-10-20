b84a450fc09c6697dae6f939d35c5f6ae0b967d9
DEVita42 <dvt.lorenzo@gmail.com>
2024-10-20 18:57:24 +0200

    data-fetching-2 bugfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import { GithubUser } from './GithubUser'
+import { GithubUsers } from './GithubUsers'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <GithubUser/>
+      <GithubUsers/>
     </>
   )
 }
